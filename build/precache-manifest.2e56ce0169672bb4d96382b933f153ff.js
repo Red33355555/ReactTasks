834d0f4144542e1d67f78128672d9551dab7df7a
Sohan Varnikar <58203414+Red33355555@users.noreply.github.com>
2020-07-05 15:15:43 +0530

    Add files via upload

---
+++ build/precache-manifest.2e56ce0169672bb4d96382b933f153ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d719390ca9060d28f622dd5af252dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03aafe06ef3e51511008",
+    "url": "/static/css/main.c274b98b.chunk.css"
+  },
+  {
+    "revision": "4a8c88263576b32f3c3f",
+    "url": "/static/js/2.8b96f628.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8b96f628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03aafe06ef3e51511008",
+    "url": "/static/js/main.79d6abe1.chunk.js"
+  },
+  {
+    "revision": "a87051cab278ef87f339",
+    "url": "/static/js/runtime-main.eaa98dab.js"
+  }
+]);
